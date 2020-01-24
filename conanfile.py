a98b7cca55302edd3d1719c2ad57891d06322ac4
Rohan Sahay <rohansahay@elear.solutions>
2020-01-24 19:45:18 +0530

    fix dependencies

--- conanfile.py
+++ conanfile.py
@@ -141,8 +141,8 @@ class CurllibConan(ConanFile):
         self.run("cd .. && autoreconf -fsi")
         args = self.config_options()
         if self.options.with_openssl and (not self.options.with_darwinssl and not self.options.with_winssl ):
-            self.requires.add("openssl/0.0.1@jenkins/master", private= False)
-            args.append("--with-ssl={!s}".format(self.deps_cpp_info["openssl"].rootpath))
+            self.requires.add("OpenSSL/1.0.2r@jenkins/master", private= False)
+            args.append("--with-ssl={!s}".format(self.deps_cpp_info["OpenSSL"].rootpath))
         args.append("--with-zlib={!s}".format(self.deps_cpp_info["zlib"].rootpath)) if self.options.with_zlib else args.append("--without-zlib")
         query = "%s-%s-%s" % (self.settings.os, self.settings.arch, self.settings.compiler)
         ancestor = next((self._targets[i] for i in self._targets if fnmatch.fnmatch(query, i)), None)
